48c53139db6cbfc2f6b9d76f6ba4db74c9a7e301
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-22 15:07:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each do |person|
+    messages << badge_maker(person)
+  end
+  messages
+end
+
+def assign_rooms(speakers)
+  messages = []
+  speakers.each_with_index do |person, room|
+    messages << "Hello, #{person}! You'll be assigned to room #{room + 1}!"
+  end
+  messages
+  binding.pry
+end
+
+def printer(attendees)
+  list_of_badges = []
+  binding.pry
+end
+  
